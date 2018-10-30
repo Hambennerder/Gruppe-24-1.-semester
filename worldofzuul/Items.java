05bdefe931047ee3530d52888383b0dba417165f
NaimoIbrahim75 <43139535+NaimoIbrahim75@users.noreply.github.com>
2018-10-30 08:54:56 +0100

    Add files via upload

---
+++ worldofzuul/Items.java
@@ -0,0 +1,38 @@
+package sduul;
+
+    public class Items{
+         
+        private String name;
+        private String info;
+        private int value;
+        private int weight;
+         
+    public Items(String name, String info, int value){
+         
+        this.name = name;
+        this.info = info;
+        this.value = value;
+         
+    }
+    public String getName(){
+         
+        return name;
+         
+    }
+    public String getInfo(){
+         
+        return info;
+     
+    }
+    public int getValue(){
+         
+        return value;
+     
+    }
+    public int getWeight(){
+        
+        return weight;
+    }
+  }
+
+
