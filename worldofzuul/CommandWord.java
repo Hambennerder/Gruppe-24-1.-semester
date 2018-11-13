bc89b5f12c22b0606ad8af5d4bd61c62ae18aac0
NaimoIbrahim75 <43139535+NaimoIbrahim75@users.noreply.github.com>
2018-11-13 09:09:51 +0100

    Update CommandWord.java

--- worldofzuul/CommandWord.java
+++ worldofzuul/CommandWord.java
@@ -1,18 +1,15 @@
 package worldofzuul;
 
-public enum CommandWord
-{
+public enum CommandWord{
     GO("go"), QUIT("quit"), HELP("help"), TAKE("take"), EQUIP("equip"), DROP("drop"), UNKNOWN("?");
     
     private String commandString;
     
-    CommandWord(String commandString)
-    {
+    CommandWord(String commandString){
         this.commandString = commandString;
     }
     
-    public String toString()
-    {
+    public String toString(){
         return commandString;
     }
 }
